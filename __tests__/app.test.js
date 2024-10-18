055afdaf4c9f9811cec40baa4721e62957c97ac8
xon-ac <shawn.clarke@icloud.com>
2024-10-18 12:30:49 +0200

    started testing suite

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 const app = require("../app");
 const request = require('supertest');
+const users = require('./testData/users');
 
 const products = require('../db/data/test-data/products');
 const users = require('../db/data/test-data/users');
@@ -7,16 +8,53 @@ const reviews = require('../db/data/test-data/reviews');
 const orders = require('../db/data/test-data/orders');
 const addresses = require('../db/data/test-data/adresses');
 
-describe('Product Routes', () => {
+describe('Product Endpoints', () => {
   it('should fetch all products', async () => {
     const res = await request(app).get('/products');
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('products');
+    expect(res.body.length).toBe(products.length);
   });
 
   it('should fetch a single product by ID', async () => {
     const res = await request(app).get('/products/1');
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('id', 1);
+    expect(res.body).toHaveProperty('name', products[0].name);
+  });
+
+  it('should return 404 if product not found', async () => {
+    const res = await request(app).get('/products/999'); 
+    expect(res.statusCode).toEqual(404);
   });
 });
+
+describe('User Endpoints', () => {
+  it('should fetch user profile by ID', async () => {
+    const res = await request(app).get('/users/1');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('name', users[0].name);
+    expect(res.body).toHaveProperty('email', users[0].email);
+  });
+
+  it('should return 404 if user not found', async () => {
+    const res = await request(app).get('/users/999');
+    expect(res.statusCode).toEqual(404);
+  });
+});
+
+describe('Address Endpoints', () => {
+  it('should fetch all addresses for a user', async () => {
+    const res = await request(app).get('/users/1/addresses');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.length).toBe(2); 
+    expect(res.body[0]).toHaveProperty('postalCode', addresses[0].postalCode);
+  });
+
+  it('should return 404 if user has no addresses', async () => {
+    const res = await request(app).get('/users/999/addresses');
+    expect(res.statusCode).toEqual(404);
+  });
+});
+
+
+
+
